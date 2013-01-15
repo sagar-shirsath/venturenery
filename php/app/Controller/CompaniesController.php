b5f88b2732d4b4e3fd32a9d084c8ca102bb9077e
Sagar Shirsath <sagar.shirsath2009@gmail.com>
2013-01-16 00:02:03 +0530

    model changes

--- php/app/Controller/CompaniesController.php
+++ php/app/Controller/CompaniesController.php
@@ -160,7 +160,7 @@ class CompaniesController extends AppController {
 //            $this->Company->Employee->create();
 //            $employee['Employee']['data_fetch_url']="api.crunchbase.com/v/1/person/"
             echo "Data saved";
-            sleep(0.10);
+
         }
 
 
